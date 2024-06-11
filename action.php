bea7c8427667f4c44579f4ae5db7970e1abada97
pooja <poosonu622@gmail.com>
2024-06-11 20:49:54 +0530

    first commit

--- action.php
+++ action.php
@@ -1,4 +1,6 @@
 <?php
+
+//let's understand how to count repeated letter in given string 
 $str =str_replace(' ', '', 'MuKesh Singh');
 $arr =str_split(strtolower($str));
 
@@ -12,4 +14,20 @@ foreach($arr as $val){
 }
 
 print_r($array);
+
+//let's understand how to short array without using any inbuild function 
+
+$arrashort =[1,3,1,3,2,99,8,99,30];
+
+for($i =0; $i < count($arrashort); $i++){
+	for($j =0; $j < count($arrashort) -1; $j++){
+		if($arrashort[$j] > $arrashort[$j +1]){
+			$temp = $arrashort[$j];
+			$arrashort[$j]  = $arrashort[$j + 1];
+			$arrashort[$j + 1] = $temp;
+		}
+	}
+}
+print_r($arrashort);
+
 ?>
